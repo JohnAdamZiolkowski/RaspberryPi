74cae89fd2375ec85b09a7bfad75cdea32ba0c7a
John Adam Ziolkowski <johnadamziolkowski@gmail.com>
2018-07-07 15:03:03 +0000

    pico8: buffer input on all 8 buttons

--- pico8.py
+++ pico8.py
@@ -1,16 +1,52 @@
 import RPi.GPIO as gpio
 
-red = 16
-button = 33
+left = 11
+right = 13
+up = 15
+down = 16
+a = 18
+b = 22
+select = 29
+start = 32
+last_state = {
+    "left": False,
+    "right": False,
+    "up": False,
+    "down": False,
+    "A": False,
+    "B": False,
+    "select": False,
+    "start": False
+}
 
 gpio.setmode(gpio.BOARD)
 gpio.setwarnings(False)
-gpio.setup(red, gpio.OUT)
-gpio.setup(button, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(left, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(right, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(up, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(down, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(a, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(b, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(select, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(start, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-gpio.output(red, True)
 
 while True:
-    light = gpio.input(button)
-    gpio.output(red, light)
-    
+    state = {
+        "left": gpio.input(left),
+        "right": gpio.input(right),
+        "up": gpio.input(up),
+        "down": gpio.input(down),
+        "A": gpio.input(a),
+        "B": gpio.input(b),
+        "select": gpio.input(select),
+        "start": gpio.input(start)
+    }
+    for button in state:
+        if state[button] != last_state[button]:
+            if state[button]:
+                print("pressed " + button)
+            else:
+                print("released " + button)
+
+    last_state = state
